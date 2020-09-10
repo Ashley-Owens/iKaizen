17e8c7be1340cb7acbafe0df2914bc974a90da55
liu-ronny <54465266+liu-ronny@users.noreply.github.com>
2020-09-10 15:55:06 -0700

    Add passport authentication

--- server/models/connectionTest.js
+++ server/models/connectionTest.js
@@ -2,6 +2,7 @@ const env = require("../config/environment");
 const mongoose = require("mongoose");
 const Entry = require("./entry");
 const User = require("./user");
+const Habit = require("./habit");
 
 mongoose.connect(env.MONGODB_URI, {
   useNewUrlParser: true,
@@ -16,6 +17,12 @@ mongoose.connect(env.MONGODB_URI, {
     name: "test",
     username: "tester",
     passwordHash: "supersecret",
+  });
+  await Habit.create({
+    name: "some habit",
+    progress: 0,
+    isBinary: true,
+    isArchived: false,
   });
 })()
   .then((result) => {
