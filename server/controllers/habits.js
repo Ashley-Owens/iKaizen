5cfe527c34df41acaf8df30e9f8324e43b4ebc19
Sriram Narayanan <sriram210@yahoo.in>
2020-09-10 14:45:31 -0700

    habit schema done

--- server/controllers/habits.js
+++ server/controllers/habits.js
@@ -3,5 +3,7 @@ const habitsRouter = require("express").Router();
 // TODO
 
 // Sriram
-
+habitsRouter.get("/",/*fill here*/, async (req, res) => {
+    let page = parseInt(req.query.page)
+})
 module.exports = habitsRouter;
--- server/models/habit.js
+++ server/models/habit.js
@@ -1,3 +1,36 @@
-// TODO
-
 // Sriram
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const formatDate = require("../utils/formatDate");
+
+const habitSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  progress: { type: Number, required: true, default:0, min:0, max:100},
+  date: { type: Date, default: Date.now },
+  isBinary: {type: Boolean, required: true},
+  isArchived: {type: Boolean, required},
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  entries: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Entry",
+    },
+  ],
+});
+
+habitSchema.set("toJSON", {
+  transform: (document, returnedObect) => {
+    returnedObect.id = returnedObject._id.toString();
+    delete returnedObect._id;
+    delete returnedObect.__v;
+
+    returnedObject.date = formatDate(returnedObject.date);
+  },
+});
+
+habitSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Habit", habitSchema);
