108ae80fc9ab96a1e12620d181afb2e6cd9cc4cd
Santiago Monleon <monleonsdevelopment@gmail.com>
2020-09-10 14:40:50 -0700

    Updated entry

--- server/models/entry.js
+++ server/models/entry.js
@@ -4,7 +4,6 @@ const uniqueValidator = require("mongoose-unique-validator");
 const formatDate = require("../utils/formatDate");
 
 const entrySchema = new mongoose.Schema({
-<<<<<<< HEAD
     emotion: { type: String, required: true },
     habitSelected: {type: mongoose.Schema.Types.ObjectId, ref: "Habit"},
     date: {type: Date, required: true, default: Date.now},
@@ -13,17 +12,6 @@ const entrySchema = new mongoose.Schema({
       ref: "User",
       required: true,
     },
-=======
-  emotion: { type: String, unique: false, required: true },
-  habitSelected: { type: String, unique: false, required: false },
-  date: { type: Date, unique: true, required: true, default: Date.now },
-  isActive: { type: Boolean, unique: false, required: true, default: true },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
->>>>>>> e0375ffd5689c03a17472cc5527d66031e86d6a7
 });
 
 entrySchema.set("toJSON", {
