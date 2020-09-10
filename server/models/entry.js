e0375ffd5689c03a17472cc5527d66031e86d6a7
liu-ronny <54465266+liu-ronny@users.noreply.github.com>
2020-09-10 12:54:04 -0700

    Create user schema

--- server/models/entry.js
+++ server/models/entry.js
@@ -1,28 +1,28 @@
 const env = require("../config/environment");
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
-const { formatDate } = require("../utils/utils");
+const formatDate = require("../utils/formatDate");
 
 const entrySchema = new mongoose.Schema({
-    emotion: { type: String, unique: false, required: true },
-    habitSelected: {type:String, unique: false, required: false},
-    date: {type: Date, unique: true, required: true, default: Date.now},
-    isActive: {type: Boolean, unique: false, required: true, default: true},
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+  emotion: { type: String, unique: false, required: true },
+  habitSelected: { type: String, unique: false, required: false },
+  date: { type: Date, unique: true, required: true, default: Date.now },
+  isActive: { type: Boolean, unique: false, required: true, default: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 entrySchema.set("toJSON", {
-    transform: (document, returnedObect) => {
-        returnedObect.id = returnedObject._id.toString();
-        delete returnedObect._id;
-        delete returnedObect.__v;
+  transform: (document, returnedObect) => {
+    returnedObect.id = returnedObject._id.toString();
+    delete returnedObect._id;
+    delete returnedObect.__v;
 
-        returnedObject.date = formateDate(returnedObject.date);
-    }
+    returnedObject.date = formatDate(returnedObject.date);
+  },
 });
 
 entrySchema.plugin(uniqueValidator);
