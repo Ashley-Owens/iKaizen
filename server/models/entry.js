2d706f276ab446dd84bbb62d69b2e2d030d37640
Santiago Monleon <monleonsdevelopment@gmail.com>
2020-09-10 12:39:55 -0700

    Created entry schema

--- server/models/entry.js
+++ server/models/entry.js
@@ -1,1 +1,30 @@
-var test = "test";
+const env = require("../config/environment");
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const { formatDate } = require("../utils/utils");
+
+const entrySchema = new mongoose.Schema({
+    emotion: { type: String, unique: false, required: true },
+    habitSelected: {type:String, unique: false, required: false},
+    date: {type: Date, unique: true, required: true, default: Date.now},
+    isActive: {type: Boolean, unique: false, required: true, default: true},
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+});
+
+entrySchema.set("toJSON", {
+    transform: (document, returnedObect) => {
+        returnedObect.id = returnedObject._id.toString();
+        delete returnedObect._id;
+        delete returnedObect.__v;
+
+        returnedObject.date = formateDate(returnedObject.date);
+    }
+});
+
+entrySchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Entry", entrySchema);
