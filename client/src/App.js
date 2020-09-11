49be289f8aa0e06b1123cec41e46f35ab5476005
booleancube <ashleyowenspt@gmail.com>
2020-09-11 16:29:02 -0700

    Set up router in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button} from 'react-bootstrap';
 import NavBar from './components/NavBar';
@@ -6,35 +12,89 @@ import MyModal from './components/MyModal';
 import Greeter from './components/Greeter';
 import './App.css';
 import Footer from './components/Footer';
+import Dashboard from './components/Dashboard';
+import About from './components/About';
 
 
-function App() {
+export default function App() {
   const [modalShow, setModalShow] = React.useState(false);
+  
+  
   return (
-    
-    <div className="App">
-      <header className="App-header">
-      <div id="content">
-        <NavBar/>
-        <>
-        <Button variant="info" onClick={() => setModalShow(true)}>
-          Sign Up
-        </Button>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="Dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
 
-        <MyModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-        />
-      </>
-        <Greeter name="Ashley"/>
-        </div>
-      </header>
-      
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/Dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+    
+    
+    // <Router>
+    // <div className="App">
+    //   <header className="App-header">
+    //   <div id="content">
+    //     <NavBar/>
+    //     <>
+    //     <Button variant="info" onClick={() => setModalShow(true)}>
+    //       Sign Up
+    //     </Button>
+
+    //     <MyModal
+    //       show={modalShow}
+    //       onHide={() => setModalShow(false)}
+    //     />
+    //   </>
+    //     <Greeter name="Ashley"/>
+    //     {/* A <Switch> looks through its children <Route>s and
+    //         renders the first one that matches the current URL. */}
+    //     <Switch>
+    //       <Route path="components/about">
+    //         <About />
+    //       </Route>
+    //       <Route path="components/Dashboard">
+    //         <Dashboard />
+    //       </Route>
+    //       <Route path="/">
+    //         <Home />
+    //       </Route>
+    //     </Switch>
+    //     </div>
+    //   </header>
        
-        <Footer />
-          
-      </div>
+    //     <Footer />
+    //   </div>
+    // </Router>
   );
 }
 
-export default App;
+
+    
+
+function Home() {
+  return <h2>I'm the Home page</h2>;
+}
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Navbar, Nav, Form, Button} from 'react-bootstrap';
 import './NavBar.css';
+import './About';
 
 
 function NavBar() {
@@ -8,9 +9,9 @@ function NavBar() {
         
         <Navbar bg="dark" variant="dark">
             <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="./MyModal.jsx">Sign Up</Nav.Link>
+            <Nav.Link href="./">Home</Nav.Link>
+            <Nav.Link href="./About">About</Nav.Link>
+            <Nav.Link href="./MyModal">Sign Up</Nav.Link>
             {/* <Navbar.Brand href="#home">iKaizen</Navbar.Brand> */}
             </Nav>
             <Form inline>
