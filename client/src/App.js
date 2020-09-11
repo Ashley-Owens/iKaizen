9254736408f55f89904fda2f167f8624fea5c5d1
booleancube <ashleyowenspt@gmail.com>
2020-09-10 18:02:33 -0700

    Created a navbar component, added to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from 'react-bootstrap';
+import NavBar from './components/NavBar';
+import Greeter from './components/Greeter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div>
+        <NavBar/>
+        <Greeter name="Ashley"/>
+        
+        <Button>Test</Button>
+      </div>    
       </header>
     </div>
   );
