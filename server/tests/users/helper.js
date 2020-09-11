aa20ff0e6a63ec34de6a2ec53135ea69838d6603
liu-ronny <54465266+liu-ronny@users.noreply.github.com>
2020-09-10 22:33:24 -0700

    create test files for routers

---
+++ server/tests/users/helper.js
@@ -0,0 +1,159 @@
+const User = require("../../models/user");
+const {
+  initialUsers,
+  initialUsersWithHashedPasswords,
+} = require("./fakeUsers");
+
+const insertInitialUsers = async () => {
+  await User.create(initialUsersWithHashedPasswords);
+};
+
+const users = () => {
+  return initialUsers.map((user) => {
+    return { ...user };
+  });
+};
+
+const usersWithHashedPasswords = () => {
+  return initialUsersWithHashedPasswords.map((user) => {
+    return { ...user };
+  });
+};
+
+const nonExistentId = async () => {
+  const user = {
+    firstName: "hello",
+    lastName: "world",
+    username: "willsoon",
+    passwordHash: "notexist",
+  };
+  const newUser = await User.create(user);
+
+  const id = newUser._id;
+
+  await User.findByIdAndDelete(id);
+
+  return id;
+};
+
+const user = () => {
+  return { ...initialUsers[0] };
+};
+
+const userInDb = async () => {
+  return await User.findOne({ username: "user" });
+};
+
+const usersInDb = async () => {
+  return await User.find({});
+};
+
+const deleteAll = async () => {
+  await User.deleteMany({});
+};
+
+const parseCookie = (cookie) => {
+  const parsedCookie = {};
+  cookie = cookie.split(";").map((prop) => prop.split("="));
+
+  for (let [key, val] of cookie) {
+    key = key.trim();
+    val = val ? val.trim() : true;
+
+    parsedCookie[key] = val;
+  }
+
+  return parsedCookie;
+};
+
+const login = async (api, username, password) => {
+  const response = await api
+    .post("/api/users/login")
+    .send({ username, password })
+    .expect(200);
+
+  const sessionId = parseCookie(response.header["set-cookie"][0])[
+    "connect.sid"
+  ];
+
+  return sessionId;
+};
+
+const logout = async (api, sessionId) => {
+  const request = api.get("/api/users/logout");
+
+  if (sessionId) {
+    request.set("Cookie", `connect.sid=${sessionId}`);
+  }
+
+  await request.expect(200);
+};
+
+const createUser = async (api, user, statusCode) => {
+  const { firstName, lastName, username, password } = user;
+  const request = api.post("/api/users");
+
+  if (firstName) request.send({ firstName });
+  if (lastName) request.send({ lastName });
+  if (username) request.send({ username });
+  if (password) request.send({ password });
+  request.expect(statusCode);
+
+  return (await request).body;
+};
+
+const editUser = async (api, sessionId, userEdit, statusCode) => {
+  const request = api.put("/api/users/myself");
+
+  if (sessionId) {
+    request.set("Cookie", `connect.sid=${sessionId}`);
+  }
+
+  request.send(userEdit).expect(statusCode);
+
+  return (await request).body;
+};
+
+// const checkSession = async (api, sessionId) => {
+//   const request = api.get("/api/sessions");
+
+//   if (sessionId) {
+//     request.set("Cookie", `connect.sid=${sessionId}`);
+//   }
+
+//   const response = await request.expect(200);
+//   const { authenticated } = response.body;
+
+//   return authenticated;
+// };
+
+// const getUserBlogs = async (api, statusCode, sessionId) => {
+//   const request = api.get("/api/users/my/blogs");
+
+//   if (sessionId) {
+//     request.set("Cookie", `connect.sid=${sessionId}`);
+//   }
+
+//   const response = await request.expect(statusCode);
+//   const blogs = response.body;
+
+//   return blogs;
+// };
+
+module.exports = {
+  insertInitialUsers,
+  user,
+  users,
+  usersWithHashedPasswords,
+  userInDb,
+  usersInDb,
+  deleteAll,
+  parseCookie,
+  login,
+  logout,
+  //   checkSession,
+  //   getUserBlogs,
+  nonExistentId,
+  createUser,
+  editUser,
+};
---
+++ server/tests/users/users.test.js
@@ -0,0 +1,211 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../../app");
+
+const api = supertest(app);
+const helper = require("./helper");
+
+beforeEach(async () => {
+  await helper.deleteAll();
+  await helper.insertInitialUsers();
+});
+
+afterEach(async () => {
+  await helper.deleteAll();
+});
+
+afterAll(async () => {
+  await mongoose.connection.db.collection("sessions").deleteMany({});
+  mongoose.connection.close();
+});
+
+const checkUserCount = async (newUser, shouldIncrease) => {
+  const usersInDb = await helper.usersInDb();
+  const initialLength = helper.users().length;
+
+  if (shouldIncrease) {
+    expect(usersInDb).toContainEqual(expect.objectContaining(newUser));
+    expect(usersInDb).toHaveLength(initialLength + 1);
+  } else {
+    if (newUser) {
+      expect(usersInDb).not.toContainEqual(expect.objectContaining(newUser));
+    }
+    expect(usersInDb).toHaveLength(initialLength);
+  }
+};
+
+describe("creating users", () => {
+  test("works correctly when the required properties are provided", async () => {
+    const user = {
+      firstName: "first",
+      lastName: "last",
+      username: "myuser",
+      password: "mypassword",
+    };
+
+    const response = await helper.createUser(api, user, 201);
+    const newUser = response.body;
+
+    await checkUserCount(
+      { firstName: newUser.firstName, newUser: user.lastName },
+      true
+    );
+  });
+
+  test("fails with status code 400 when one of the required properties is missing", async () => {
+    // provide a user with no properties
+    let response = await helper.createUser(api, {}, 400);
+    expect(response.body.error).toBeDefined();
+
+    const user = {
+      firstName: "first",
+      lastName: "last",
+      username: "myuser",
+      password: "mypassword",
+    };
+    const properties = ["firstName", "lastName", "username", "password"];
+
+    // provide a user with one of the required properties missing
+    for (let property of properties) {
+      const userCopy = { ...user };
+      delete userCopy[property];
+
+      response = await helper.createUser(api, userCopy, 400);
+      expect(response.body.error).toBeDefined();
+    }
+  });
+
+  test("fails with status code 400 when the username is not unique", async () => {
+    const existingUser = helper.user();
+
+    response = await helper.createUser(api, existingUser, 400);
+    expect(response.body.error).toBeDefined();
+  });
+});
+
+describe("editing users", () => {
+  test("works when the request is valid and the user is logged in", async () => {
+    const user = helper.user();
+    const sessionId = await helper.login(api, user.username, user.password);
+
+    const edits = {
+      firstName: "new first name",
+      lastName: "new last name",
+      email: "newemail@gmail.com",
+      appleId: "abcde",
+    };
+    const properties = Object.keys(edits);
+
+    // try to make every combination of edits
+    for (let i = 0; i < properties.length; i++) {
+      const editCombination = {};
+
+      for (let j = i; j < properties.length; j++) {
+        property = properties[j];
+        value = edits[property];
+
+        await helper.editUser(api, sessionId, editCombination, 204);
+
+        const userInDb = await helper.userInDb();
+
+        for (let [editProperty, editValue] of Object.entries(editCombination)) {
+          expect(userInDb[editProperty]).toBe(editValue);
+        }
+      }
+    }
+  });
+
+  test("fails with status code 400 if no required property is provided", async () => {
+    const invalidEdit = {
+      dateCreated: "today",
+      reoccurringHabits: "random value",
+      longTermHabits: "another value",
+    };
+
+    const user = helper.user();
+    const sessionId = await helper.login(api, user.username, user.password);
+
+    const response = await helper.editUser(api, sessionId, invalidEdit, 400);
+    expect(response.body.error).toBeDefined();
+  });
+
+  test("fails with status code 401 if the user is not logged in", async () => {
+    const userEdit = {
+      firstName: "new first name",
+      lastName: "new last name",
+      email: "newemail@gmail.com",
+      appleId: "abcde",
+    };
+
+    const response = await helper.editUser(api, null, userEdit, 401);
+    expect(response.body.error).toBeDefined();
+  });
+});
+
+// describe("getting entries belonging to a logged in user", async () => {
+//     test("returns the current month's entries by default when there are no query parameters", async () => {
+
+//     });
+// });
+
+describe("user login", () => {
+  test("works when the user provides valid credentials", async () => {
+    const user = helper.user();
+    const sessionId = await helper.login(api, user.username, user.password);
+    expect(sessionId).toBeDefined();
+  });
+
+  test("fails with status code 401 if the user does not provide valid credentials", async () => {
+    const response = await api
+      .post("/api/users/login")
+      .send({ username: "invalid", password: "superinvalid" })
+      .expect(401);
+    expect(response.body.error).toBeDefined();
+  });
+
+  test("fails with status code 400 if either the 'username' or 'password' property is not provided", async () => {
+    let response = await api
+      .post("/api/users/login")
+      .send({ username: "user" })
+      .expect(400);
+    expect(response.body.error).toBeDefined();
+
+    response = await api
+      .post("/api/users/login")
+      .send({ password: "testuser" })
+      .expect(400);
+    expect(response.body.error).toBeDefined();
+
+    response = await api.post("/api/users/login").expect(400);
+    expect(response.body.error).toBeDefined();
+  });
+});
+
+describe("user logout", () => {
+  test("works when users are logged in", async () => {
+    jest.setTimeout(30000);
+
+    const users = helper.users();
+
+    for (let user of users) {
+      const sessionId = await helper.login(api, user.username, user.password);
+      let isLoggedIn = await helper.checkSession(api, sessionId);
+      expect(isLoggedIn).toBe(true);
+
+      await helper.logout(api, sessionId);
+      isLoggedIn = await helper.checkSession(api, sessionId);
+      expect(isLoggedIn).toBe(false);
+    }
+  });
+
+  test("works when users are not logged in", async () => {
+    // send a request to log out without sending a session id
+    await helper.logout(api);
+    let isLoggedIn = await helper.checkSession(api);
+    expect(isLoggedIn).toBe(false);
+
+    await helper.logout(api, "invalid");
+    isLoggedIn = await helper.checkSession(api, "invalid");
+    expect(isLoggedIn).toBe(false);
+  });
+});
