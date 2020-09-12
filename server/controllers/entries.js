a2b60f687e12a47d6ab8de9e3667e7e627f7846d
Santiago Monleon <monleonsdevelopment@gmail.com>
2020-09-11 21:21:23 -0700

    Testing middlewear

--- server/controllers/entries.js
+++ server/controllers/entries.js
@@ -4,25 +4,12 @@ const validator = require("validator");
 const loginRequired = require("../middleware/loginRequired");
 const Habit = require("../models/habit");
 const Entry = require("../models/entry");
+const idCheck = require("../middleware/idCheck");
 
 entriesRouter.get("/:id", loginRequired, async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    var entry = await Entry.findById(id);
-  } catch (error) {
-    return res.status(400).json({ error: "Invalid ID" });
-  }
-
-  if (!entry) {
-    return res.status(404).json({ error: "No such entry exists" });
-  }
-
-  if (req.user._id.toString() !== entry.user.toString()) {
-    return res.status(401).json({ error: "ID not found" });
-  }
-
-  res.status(200).json(entry);
+    idCheck(req, res, next, "entry");
+}, async (req, res) => {
+    res.status(200).json(entry);
 });
 
 /*
